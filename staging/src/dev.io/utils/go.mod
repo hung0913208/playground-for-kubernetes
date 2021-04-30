118aa3616858aa648ee50bc2785e081f152f9b95
Hung Nguyen Xuan Pham <hung0913208@gmail.com>
2021-04-30 08:39:22 +0700

    [TEST] add test start/stop server
    Signed-off-by: Hung Nguyen Xuan Pham <hung0913208@gmail.com>

--- staging/src/dev.io/utils/go.mod
+++ staging/src/dev.io/utils/go.mod
@@ -3,7 +3,7 @@ module dev.io/cloud/utils
 go 1.13
 
 require (
-	github.com/grpc/grpc-go v1.37.0
+	google.golang.org/grpc v1.37.0
 	github.com/gorilla/mux v1.8.0
 	github.com/graphql-go/graphql v0.7.9
 )
--- test/muxer.go
+++ test/muxer.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+  "dev.io/cloud/utils"
+  "net/http"
+  "testing"
+  "time"
+  "log"
+  "fmt"
+)
+
+func start() *http.Server {
+  r := utils.NewApiServer()
+
+  srv := &http.Server{
+    Addr: "0.0.0.0:8080",
+    WriteTimeout: time.Second * 15,
+    ReadTimeout: time.Second * 15,
+    IdleTimeout: time.Second * 60,
+    Handler: r.GetMuxer(),
+  }
+
+  go func() {
+    if err := srv.ListenAndServe(); err != nil {
+      log.Println(err)
+    }
+  }()
+}
+
+func stop(srv *http.Server) {
+  ctx, cancel := context.WithTimeout(context.Background(), wait)
+  defer cancel()
+
+  srv.Shutdown(ctx)
+  log.Println("shutting down")
+}
+
+func TestStartStopServer(t *testing.T) {
+  // start a new server
+  start()
+
+  // wait 10 seconds
+  time.Sleep(10 * time.Second)
+
+  // stop server grateful
+  stop()
+}
