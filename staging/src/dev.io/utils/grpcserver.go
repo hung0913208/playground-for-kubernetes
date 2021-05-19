df7cb895d66e7c70cb91c80bbb0814e5a8f6ab90
Hung Nguyen Xuan Pham <hung0913208@gmail.com>
2021-05-20 05:33:09 +0700

    [DRAFT] codebase to create grpc connection
    Signed-off-by: Hung Nguyen Xuan Pham <hung0913208@gmail.com>

---
+++ staging/src/dev.io/utils/grpcserver.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+  "google.golang.org/grpc"
+  "net"
+
+)
+
+type Inventory interface {
+  Version() string
+  New(conn *grpc.ClientConn) error
+}
+
+type Implementer interface {
+  Version() string
+  New(serv *grpc.Server) error
+}
+
+/*! \brief Connect inventory to implementer
+ *
+ *  This function is used to connect our inventory to remote implementer
+ * before doing anything
+ *
+ *  \return error: if everything ok, we will receive nil object otherwide we
+ *                 will receive error which indicate issue during connecting
+ */
+func (self Inventory) Connect(conn *grpc.ClientConn) error {
+  return self.New(conn)
+}
+
+/*! \brief Serve an implementer to resolve requests
+ *
+ *  This function is used to start on-board our implementer to serve requests
+ * from clients
+ *
+ *  \return error: if everything ok, we will receive nil object otherwide we
+ *                 will receive error which indicate issue during connecting
+ */
+func (self Implementer) Serve(lis net.Listener) error {
+  serv := grpc.NewServer()
+
+  if err := self.New(serv); err != nil {
+    return err
+  } else if err := serv.Serv(list); err != nil {
+    return err
+  } else {
+    return nil
+  }
+}
